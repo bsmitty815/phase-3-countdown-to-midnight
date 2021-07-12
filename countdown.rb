b2206601d9c11717b1b49bff77905edfe192db03
Brian Smith <brian.bubbam@gmail.com>
2021-07-12 16:00:57 -0500

    commit tree c421c66bc56ca23d231e679168e8567e66ac0d4e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,30 @@
 #write your code here
 
-def countdown
+
+
+def countdown(number)
+    
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(countdown)
+    while countdown >= 1
+        puts "#{countdown} SECOND(S)!"
+        countdown -= 1
+        sleep (1)
+    end
+    
+    
+    return "HAPPY NEW YEAR!"
+end
+
+
+
+
+
+
